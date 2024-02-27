1a8e933d1ca0ed828b9aa6fefb8cae8a49fb5710
SoniaTad <92749781+SoniaTad@users.noreply.github.com>
2024-02-27 04:08:16 +0000

    Altered MarengoLab directory

--- MarengoLab/Lab/context_processors.py
+++
@@ -1,8 +0,0 @@
-from .models import Patient, Sample
-from datetime import datetime
-def patient_count(request):
-    pending_count = Sample.objects.filter(status='P').count()
-    patient_count = Patient.objects.count()
-    current_date = datetime.now().date()
-    context= {'pending_count': pending_count, 'patient_count':patient_count,'current_date':current_date}
-    return context
--- MarengoLab/Lab/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms 
-from .models import Patient
-
-class RegisterPatientForm(forms.ModelForm):
-    class Meta:
-        model = Patient
-        fields = ['f_name', 'l_name', 'age', 'gender', 'email', 'tel_num', 'comment']
